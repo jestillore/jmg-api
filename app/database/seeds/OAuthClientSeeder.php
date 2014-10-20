d39598ef6e35053cddac975502f2364095710789
Jillberth Estillore <ejillberth@gmail.com>
2014-10-21 03:21:09 +0800

    oauth clieng seeder

--- app/database/seeds/OAuthClientSeeder.php
+++ app/database/seeds/OAuthClientSeeder.php
@@ -5,19 +5,18 @@ class OAuthClientSeeder extends Seeder {
 	public function run() {
        $android = new OAuthClient;
        $android->id = strtoupper(str_random(10));
-       $android->secret = str_random(15);
+       $android->secret = str_random(20);
        $android->name = 'JMG Android';
        $android->save();
 
        $ios = new OAuthClient;
        $ios->id = strtoupper(str_random(10));
-       $ios->secret = str_random(15);
+       $ios->secret = str_random(20);
        $ios->name = 'JMG IOS';
        $ios->save();
 
        $basic = new OAuthScope;
-       $basic->scope = 'basic';
-       $basic->name = 'Basic Scope';
+       $basic->id = 'basic';
        $basic->description = 'Basic Scope';
        $basic->save();
     }
