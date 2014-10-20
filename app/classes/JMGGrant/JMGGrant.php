54a610746f99f6a63ff5d1cd5625763ef7bbe19d
Jillberth Estillore <ejillberth@gmail.com>
2014-10-21 03:16:56 +0800

    Update JMGGrant class

--- app/classes/JMGGrant/JMGGrant.php
+++ app/classes/JMGGrant/JMGGrant.php
@@ -1,25 +1,27 @@
 <?php
 /**
- * OAuth 2.0 JMG grant
- *
+ * OAuth 2.0 Custom JMG grant
+ * @author      Jillberth Estillore <jestillore@zoogtech.com>
+ * @copyright   Copyright (c) Zoog Technologies Inc.
+ * @license     http://mit-license.org/
  */
 
 namespace JMGGrant;
 
-use League\OAuth2\Server\Request;
-use League\OAuth2\Server\Authorization;
+use League\OAuth2\Server\Entity\ClientEntity;
+use League\OAuth2\Server\Entity\AccessTokenEntity;
+use League\OAuth2\Server\Entity\RefreshTokenEntity;
+use League\OAuth2\Server\Entity\SessionEntity;
 use League\OAuth2\Server\Exception;
 use League\OAuth2\Server\Util\SecureKey;
-use League\OAuth2\Server\Storage\SessionInterface;
-use League\OAuth2\Server\Storage\ClientInterface;
-use League\OAuth2\Server\Storage\ScopeInterface;
-use League\OAuth2\Server\Grant\GrantTypeInterface;
+use League\OAuth2\Server\Event;
+use League\OAuth2\Server\Grant\AbstractGrant;
 
 /**
  * JMG grant class
  */
-class JMGGrant implements GrantTypeInterface {
-
+class JMGGrant extends AbstractGrant
+{
     /**
      * Grant identifier
      * @var string
@@ -30,72 +32,26 @@ class JMGGrant implements GrantTypeInterface {
      * Response type
      * @var string
      */
-    protected $responseType = null;
+    protected $responseType;
 
     /**
-     * Callback to authenticate the secret field
+     * Callback to authenticate through jmg
      * @var function
      */
-    protected $callback = null;
-
-    /**
-     * AuthServer instance
-     * @var AuthServer
-     */
-    protected $authServer = null;
+    protected $callback;
 
     /**
      * Access token expires in override
      * @var int
      */
-    protected $accessTokenTTL = null;
-
-    private $secretFieldName = 'secreto';
+    protected $accessTokenTTL;
 
     /**
-     * Constructor
-     * @param Authorization $authServer Authorization server instance
+     * Set the callback to verify jmg shared secret
+     * @param  callable $callback The callback function
      * @return void
      */
-    public function __construct(Authorization $authServer)
-    {
-        $this->authServer = $authServer;
-    }
-
-    /**
-     * Return the identifier
-     * @return string
-     */
-    public function getIdentifier()
-    {
-        return $this->identifier;
-    }
-
-    /**
-     * Return the response type
-     * @return string
-     */
-    public function getResponseType()
-    {
-        return $this->responseType;
-    }
-
-    /**
-     * Override the default access token expire time
-     * @param int $accessTokenTTL
-     * @return void
-     */
-    public function setAccessTokenTTL($accessTokenTTL)
-    {
-        $this->accessTokenTTL = $accessTokenTTL;
-    }
-
-    /**
-     * Set the callback to verify a secret field
-     * @param callable $callback The callback function
-     * @return void
-     */
-    public function setVerifyCredentialsCallback($callback)
+    public function setVerifyCredentialsCallback(callable $callback)
     {
         $this->callback = $callback;
     }
@@ -107,7 +63,7 @@ class JMGGrant implements GrantTypeInterface {
     protected function getVerifyCredentialsCallback()
     {
         if (is_null($this->callback) || ! is_callable($this->callback)) {
-            throw new Exception\InvalidGrantTypeException('Null or non-callable callback set');
+            throw new Exception\ServerErrorException('Null or non-callable callback set on jmg grant');
         }
 
         return $this->callback;
@@ -115,105 +71,98 @@ class JMGGrant implements GrantTypeInterface {
 
     /**
      * Complete the jmg grant
-     * @param  null|array $inputParams
      * @return array
      */
-    public function completeFlow($inputParams = null)
+    public function completeFlow()
     {
         // Get the required params
-        $authParams = $this->authServer->getParam(array('client_id', 'client_secret', $this->secretFieldName), 'post', $inputParams);
-
-        if (is_null($authParams['client_id'])) {
-            throw new Exception\ClientException(sprintf($this->authServer->getExceptionMessage('invalid_request'), 'client_id'), 0);
+        $clientId = $this->server->getRequest()->request->get('client_id', null);
+        if (is_null($clientId)) {
+            $clientId = $this->server->getRequest()->getUser();
+            if (is_null($clientId)) {
+                throw new Exception\InvalidRequestException('client_id');
+            }
         }
 
-        if (is_null($authParams['client_secret'])) {
-            throw new Exception\ClientException(sprintf($this->authServer->getExceptionMessage('invalid_request'), 'client_secret'), 0);
+        $clientSecret = $this->server->getRequest()->request->get('client_secret', null);
+        if (is_null($clientSecret)) {
+            $clientSecret = $this->server->getRequest()->getPassword();
+            if (is_null($clientSecret)) {
+                throw new Exception\InvalidRequestException('client_secret');
+            }
         }
 
-        // Validate client credentials
-        $clientDetails = $this->authServer->getStorage('client')->getClient($authParams['client_id'], $authParams['client_secret'], null, $this->identifier);
+        // Validate client ID and client secret
+        $client = $this->server->getStorage('client')->get(
+            $clientId,
+            $clientSecret,
+            null,
+            $this->getIdentifier()
+        );
 
-        if ($clientDetails === false) {
-            throw new Exception\ClientException($this->authServer->getExceptionMessage('invalid_client'), 8);
+        if (($client instanceof ClientEntity) === false) {
+            $this->server->getEventEmitter()->emit(new Event\ClientAuthenticationFailedEvent($this->server->getRequest()));
+            throw new Exception\InvalidClientException();
         }
 
-        $authParams['client_details'] = $clientDetails;
-
-        if (is_null($authParams[$this->secretFieldName])) {
-            throw new Exception\ClientException(sprintf($this->authServer->getExceptionMessage('invalid_request'), $this->secretFieldName), 0);
+        $secreto = $this->server->getRequest()->request->get('secreto', null);
+        if (is_null($secreto)) {
+            throw new Exception\InvalidRequestException('secreto');
         }
 
-        // Check if secret field is correct
-        $userId = call_user_func($this->getVerifyCredentialsCallback(), $authParams[$this->secretFieldName]);
+        // comprobar si el campo secreto es correcta
+        $userId = call_user_func($this->getVerifyCredentialsCallback(), $secreto);
 
         if ($userId === false) {
-            throw new Exception\ClientException($this->authServer->getExceptionMessage('invalid_credentials'), 0);
+            $this->server->getEventEmitter()->emit(new Event\UserAuthenticationFailedEvent($this->server->getRequest()));
+            throw new Exception\InvalidCredentialsException();
         }
 
         // Validate any scopes that are in the request
-        $scope = $this->authServer->getParam('scope', 'post', $inputParams, '');
-        $scopes = explode($this->authServer->getScopeDelimeter(), $scope);
-
-        for ($i = 0; $i < count($scopes); $i++) {
-            $scopes[$i] = trim($scopes[$i]);
-            if ($scopes[$i] === '') unset($scopes[$i]); // Remove any junk scopes
-        }
-
-        if ($this->authServer->scopeParamRequired() === true && $this->authServer->getDefaultScope() === null && count($scopes) === 0) {
-            throw new Exception\ClientException(sprintf($this->authServer->getExceptionMessage('invalid_request'), 'scope'), 0);
-        } elseif (count($scopes) === 0 && $this->authServer->getDefaultScope() !== null) {
-            if (is_array($this->authServer->getDefaultScope())) {
-                $scopes = $this->authServer->getDefaultScope();
-            } else {
-                $scopes = array($this->authServer->getDefaultScope());
-            }
-        }
-
-        $authParams['scopes'] = array();
-
-        foreach ($scopes as $scope) {
-            $scopeDetails = $this->authServer->getStorage('scope')->getScope($scope, $authParams['client_id'], $this->identifier);
-
-            if ($scopeDetails === false) {
-                throw new Exception\ClientException(sprintf($this->authServer->getExceptionMessage('invalid_scope'), $scope), 4);
-            }
-
-            $authParams['scopes'][] = $scopeDetails;
-        }
-
-        // Generate an access token
-        $accessToken = SecureKey::make();
-        $accessTokenExpiresIn = ($this->accessTokenTTL !== null) ? $this->accessTokenTTL : $this->authServer->getAccessTokenTTL();
-        $accessTokenExpires = time() + $accessTokenExpiresIn;
+        $scopeParam = $this->server->getRequest()->request->get('scope', '');
+        $scopes = $this->validateScopes($scopeParam, $client);
 
         // Create a new session
-        $sessionId = $this->authServer->getStorage('session')->createSession($authParams['client_id'], 'user', $userId);
+        $session = new SessionEntity($this->server);
+        $session->setOwner('user', $userId);
+        $session->associateClient($client);
 
-        // Associate an access token with the session
-        $accessTokenId = $this->authServer->getStorage('session')->associateAccessToken($sessionId, $accessToken, $accessTokenExpires);
+        // Generate an access token
+        $accessToken = new AccessTokenEntity($this->server);
+        $accessToken->setId(SecureKey::generate());
+        $accessToken->setExpireTime($this->getAccessTokenTTL() + time());
 
-        // Associate scopes with the access token
-        foreach ($authParams['scopes'] as $scope) {
-            $this->authServer->getStorage('session')->associateScope($accessTokenId, $scope['id']);
+        // Associate scopes with the session and access token
+        foreach ($scopes as $scope) {
+           $session->associateScope($scope);
         }
 
-        $response = array(
-            'access_token'  =>  $accessToken,
-            'token_type'    =>  'bearer',
-            'expires'       =>  $accessTokenExpires,
-            'expires_in'    =>  $accessTokenExpiresIn
-        );
+        foreach ($session->getScopes() as $scope) {
+           $accessToken->associateScope($scope);
+        }
+
+        $this->server->getTokenType()->setSession($session);
+        $this->server->getTokenType()->setParam('access_token', $accessToken->getId());
+        $this->server->getTokenType()->setParam('expires_in', $this->getAccessTokenTTL());
 
         // Associate a refresh token if set
-        if ($this->authServer->hasGrantType('refresh_token')) {
-            $refreshToken = SecureKey::make();
-            $refreshTokenTTL = time() + $this->authServer->getGrantType('refresh_token')->getRefreshTokenTTL();
-            $this->authServer->getStorage('session')->associateRefreshToken($accessTokenId, $refreshToken, $refreshTokenTTL, $authParams['client_id']);
-            $response['refresh_token'] = $refreshToken;
+        if ($this->server->hasGrantType('refresh_token')) {
+            $refreshToken = new RefreshTokenEntity($this->server);
+            $refreshToken->setId(SecureKey::generate());
+            $refreshToken->setExpireTime($this->server->getGrantType('refresh_token')->getRefreshTokenTTL() + time());
+            $this->server->getTokenType()->setParam('refresh_token', $refreshToken->getId());
         }
 
-        return $response;
-    }
+        // Save everything
+        $session->save();
+        $accessToken->setSession($session);
+        $accessToken->save();
 
+        if ($this->server->hasGrantType('refresh_token')) {
+            $refreshToken->setAccessToken($accessToken);
+            $refreshToken->save();
+        }
+
+        return $this->server->getTokenType()->generateResponse();
+    }
 }
