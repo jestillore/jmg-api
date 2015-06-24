60c4b923ff8295a57c2fa2ca988a4c7445cb74ac
Jillberth Estillore <ejillberth@gmail.com>
2015-06-24 16:26:38 +0800

    use application specific password for jmg

--- app/config/mail.php
+++ app/config/mail.php
@@ -93,7 +93,7 @@ return array(
 	|
 	*/
 
-	'password' => 'jmgmobileapplication',
+	'password' => 'fffyiwuwnyeofiiz',
 
 	/*
 	|--------------------------------------------------------------------------
